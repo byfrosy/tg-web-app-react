bb8f383ac1db164340da8edd94b3d7f8452206cb
by.frosy <by.frosy@mail.ru>
2024-06-25 02:44:54 +0300

    1

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -6,6 +6,7 @@ const LoadingScreen = () => {
   return (
     <div className='LoadingBG'>
       <h1 className='LoadingText'>Match Prediction</h1>
+      <span className='LoadingText'>COUNTER-STIKE 2</span>
     </div>
   );
 };
