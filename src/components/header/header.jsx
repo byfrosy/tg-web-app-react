df3a8da8501226f97086b144201da11da7e0b527
by.frosy <by.frosy@mail.ru>
2024-06-24 14:36:52 +0300

    1

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -6,13 +6,30 @@ const Header = () => {
     const {user, onClose} = useTelegram();
 
     return (
+        // <div className="header">
+        //     <sapn className={'username'}>  
+        //         {user?.first_name} {user?.last_name}
+        //     </sapn>
+        //     <sapn className={'id'}>  
+        //         {user?.id}
+        //     </sapn>
+        // </div>
+
         <div className="header">
-            <sapn className={'username'}>  
-                {user?.first_name} {user?.last_name}
-            </sapn>
-            <sapn className={'id'}>  
-                {user?.id}
-            </sapn>
+            <div className="header-content">
+                <div className="profile">
+                    <img src="{user?.photo_url}" alt="Profile" className="profile-image" />
+                    <div className="profile-info">
+                        <div className="profile-name">{user?.first_name} {user?.last_name}</div>
+                        <div className="profile-id">id: {user?.id}</div>
+                    </div>
+                </div>
+                <div className="balance">
+                    <span className="balance-text">БАЛАНС</span>
+                    <span className="balance-amount">99 999 Р</span>
+                </div>
+                <button className="recharge-button">ПОПОЛНИТЬ</button>
+            </div>
         </div>
     );
 };
