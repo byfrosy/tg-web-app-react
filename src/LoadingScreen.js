55beb5e0ac66af2965331e78ca70fb0cfe0f332c
by.frosy <by.frosy@mail.ru>
2024-06-25 03:45:22 +0300

    1

--- src/LoadingScreen.js
+++ src/LoadingScreen.js
@@ -5,8 +5,8 @@ import './App.css';
 const LoadingScreen = () => {
   return (
     <div className='LoadingBG'>
-      <h1 className='LoadingTexth1'>Match Prediction</h1>
-      <span className='LoadingTextspan'>COUNTER-STIKE 2</span>
+      <span className='LoadingTextspan1'>Match Prediction</span>
+      <span className='LoadingTextspan2'>COUNTER-STIKE 2</span>
     </div>
   );
 };
