356be7c702a506af217dca0faa41c31e5708ff04
by.frosy <by.frosy@mail.ru>
2024-07-05 21:59:11 +0300

    1

---
+++ src/components/mainmenu/mainmenu.js
@@ -0,0 +1,16 @@
+// components/Footer.js
+import React from 'react';
+import './mainmenu.css';
+
+const Footer = () => {
+  return (
+    <div className="mainmenu">
+      <button className="mainmenu-button">Button 1</button>
+      <button className="mainmenu-button">Button 2</button>
+      <button className="mainmenu-button">Button 3</button>
+      <button className="mainmenu-button">Button 4</button>
+    </div>
+  );
+};
+
+export default Footer;
